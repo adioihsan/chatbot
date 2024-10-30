5231f6249d1af2a1ef722c604fc29b49b608a0ef
adio ihsan <adioihsan@gmail.com>
2024-10-30 09:24:26 +0700

    feat: add chats table migrations

---
+++ database/migrations/2024_10_30_004528_create_chat_histories_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chat_histories', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('chat_id')->references('id')->on('chats');
+            $table->text('prompt');
+            $table->text('response');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chat_histories');
+    }
+};
