df71ef0277b8ad648eb8988e18c5ad1cb8181b38
adio ihsan <adioihsan@gmail.com>
2024-11-05 13:33:01 +0700

    fix: dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head,Link } from '@inertiajs/react';
+import { TbMessageChatbotFilled } from "react-icons/tb";
 
 export default function Dashboard() {
     return (
@@ -14,12 +15,12 @@ export default function Dashboard() {
 
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">
-                            You're logged in!
-                        </div>
-                    </div>
+                        <Link className='p-3 bg-white shadow-lg w-full rounded-lg flex items-center gap-2'  href='/chat'>
+                            <TbMessageChatbotFilled />
+                            <span>ChatBot</span>
+                        </Link>
                 </div>
+            
             </div>
         </AuthenticatedLayout>
     );
